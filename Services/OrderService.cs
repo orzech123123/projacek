77b1357c308b2efbd63d076597c3bdba5bf4fa59
orzech <orzech@DESKTOP-BE8VQUO>
2021-03-23 16:39:20 +0100

    refactor

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using react_app.Configuration;
